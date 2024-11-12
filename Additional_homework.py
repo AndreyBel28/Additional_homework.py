984537ee6c291692e8ea1372b513e3eaa8a9e5a6
Андрей <omarov.a9609@gmail.com>
2024-11-12 20:14:41 +0500

    Update Additional_homework.py

--- Additional_homework.py
+++ Additional_homework.py
@@ -1,10 +1,9 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-students = ['Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron']  # изменено на список
+students = ['Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'] 
 
 students_list = sorted(students)
 
-grades_average_list = [sum(g) / len(g) for g in grades]  # использование list comprehension
-
+grades_average_list = [sum(g) / len(g) for g in grades] 
 average_grades_dict = dict(zip(students_list, grades_average_list))
 print(average_grades_dict)
 
